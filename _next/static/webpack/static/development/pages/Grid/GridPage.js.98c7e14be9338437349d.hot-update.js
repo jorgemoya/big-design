b9cd70bf669a0163db8dcffc62b51e352a49d632
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-08-15 08:15:09 -0500

    07320ef

---
+++ _next/static/webpack/static/development/pages/Grid/GridPage.js.98c7e14be9338437349d.hot-update.js
@@ -0,0 +1,205 @@
+webpackHotUpdate("static/development/pages/Grid/GridPage.js",{
+
+/***/ "./pages/Grid/GridPage.tsx":
+/*!*********************************!*\
+  !*** ./pages/Grid/GridPage.tsx ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @bigcommerce/big-design */ "../big-design/dist/big-design.es.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "../../node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../components */ "./components/index.tsx");
+/* harmony import */ var _PropTables__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../PropTables */ "./PropTables/index.ts");
+var _jsxFileName = "/Users/daniel.almaguer/dev/bigcommerce/big-design/packages/docs/pages/Grid/GridPage.tsx";
+
+
+
+
+
+var ExampleBox = function ExampleBox(_ref) {
+  var children = _ref.children;
+  return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["Box"], {
+    backgroundColor: "secondary20",
+    border: "box",
+    padding: "small",
+    style: {
+      height: '100%'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, children);
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_1___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["H0"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, "Grid"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["Text"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, "The Grid component is useful for creating intrinsicly responsive layouts.", ' ', react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["Link"], {
+    href: "https://design.bigcommerce.com/components/grid",
+    target: "_blank",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  }, "Grid Design Guidelines"), "."), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components__WEBPACK_IMPORTED_MODULE_2__["CodePreview"], {
+    scope: {
+      ExampleBox: ExampleBox
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 25
+    },
+    __self: this
+  }, "<Grid>\n  <Grid.Item>\n    <ExampleBox>Item 1</ExampleBox>\n  </Grid.Item>\n  <Grid.Item>\n    <ExampleBox>Item 2</ExampleBox>\n  </Grid.Item>\n  <Grid.Item>\n    <ExampleBox>Item 3</ExampleBox>\n  </Grid.Item>\n  <Grid.Item>\n    <ExampleBox>Item 4</ExampleBox>\n  </Grid.Item>\n</Grid>"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["H1"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 42
+    },
+    __self: this
+  }, "API"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["H2"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 44
+    },
+    __self: this
+  }, "Grid"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_PropTables__WEBPACK_IMPORTED_MODULE_3__["GridPropTable"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 46
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["H2"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 48
+    },
+    __self: this
+  }, "Grid.Item"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_PropTables__WEBPACK_IMPORTED_MODULE_3__["GridItemPropTable"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 50
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["H2"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 52
+    },
+    __self: this
+  }, "Inherited Props"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components__WEBPACK_IMPORTED_MODULE_2__["Collapsible"], {
+    title: "Box Props",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 54
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_PropTables__WEBPACK_IMPORTED_MODULE_3__["BoxPropTable"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 55
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components__WEBPACK_IMPORTED_MODULE_2__["Collapsible"], {
+    title: "Margin Props",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 58
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_PropTables__WEBPACK_IMPORTED_MODULE_3__["MarginPropTable"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 59
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components__WEBPACK_IMPORTED_MODULE_2__["Collapsible"], {
+    title: "Padding Props",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 62
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_PropTables__WEBPACK_IMPORTED_MODULE_3__["PaddingPropTable"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 63
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["H1"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 66
+    },
+    __self: this
+  }, "Examples"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["Text"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 68
+    },
+    __self: this
+  }, "Grid allows you to create custom layouts using combinations of ", react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components__WEBPACK_IMPORTED_MODULE_2__["Code"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 69
+    },
+    __self: this
+  }, "gridTemplate"), " and ", react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components__WEBPACK_IMPORTED_MODULE_2__["Code"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 69
+    },
+    __self: this
+  }, "gridArea"), ' ', "props."), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components__WEBPACK_IMPORTED_MODULE_2__["CodePreview"], {
+    scope: {
+      ExampleBox: ExampleBox
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 73
+    },
+    __self: this
+  }, "function Example() {\n  const template = `\n    \"head head\" 80px\n    \"nav  main\" 200px\n    \"foot  foot\" 50px\n    / 1fr 5fr;\n  `;\n\n  return (\n    <Grid gridTemplate={template}>\n      <Grid.Item gridArea=\"head\">\n        <ExampleBox>Header</ExampleBox>\n      </Grid.Item>\n      <Grid.Item gridArea=\"nav\">\n        <ExampleBox>Sidebar</ExampleBox>\n      </Grid.Item>\n      <Grid.Item gridArea=\"main\">\n        <ExampleBox>Content</ExampleBox>\n      </Grid.Item>\n      <Grid.Item gridArea=\"foot\">\n        <ExampleBox>Footer</ExampleBox>\n      </Grid.Item>\n    </Grid>\n  );\n}"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["Text"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 101
+    },
+    __self: this
+  }, "You can use the ", react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components__WEBPACK_IMPORTED_MODULE_2__["Code"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 102
+    },
+    __self: this
+  }, "gridColumns"), " prop to create columned layouts."), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components__WEBPACK_IMPORTED_MODULE_2__["CodePreview"], {
+    scope: {
+      ExampleBox: ExampleBox
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 105
+    },
+    __self: this
+  }, "<Grid gridColumns=\"repeat(3, 1fr)\">\n  <Grid.Item>\n    <ExampleBox>\n      Reprehenderit ullamco et elit eu duis non reprehenderit eiusmod pariatur ea deserunt irure. Reprehenderit et\n      incididunt sit aute sint proident eu eiusmod pariatur Lorem nulla labore irure nisi. Adipisicing do duis\n      occaecat ipsum dolor ea. Qui consectetur sint eu nulla duis et commodo anim commodo. Ullamco consectetur\n      elit ullamco aliquip do id consectetur anim laborum.\n    </ExampleBox>\n  </Grid.Item>\n\n  <Grid.Item>\n    <ExampleBox>\n      In sit dolore culpa reprehenderit tempor laborum. Sit anim voluptate pariatur irure amet mollit. Est mollit\n      ad pariatur esse eu reprehenderit ut veniam. Aliquip anim mollit aliquip quis officia minim esse laboris\n      proident fugiat cillum. Eiusmod culpa eu ea eiusmod nostrud.\n    </ExampleBox>\n  </Grid.Item>\n\n  <Grid.Item>\n    <ExampleBox>\n      Ipsum adipisicing pariatur magna quis incididunt amet pariatur. Aliqua voluptate consequat ut elit ea dolore\n      officia reprehenderit exercitation dolore eiusmod cupidatat quis. Ullamco ipsum do deserunt sunt ad deserunt\n      ut. Elit non velit dolore ad est eu.\n    </ExampleBox>\n  </Grid.Item>\n</Grid>"));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=GridPage.js.98c7e14be9338437349d.hot-update.js.map
---
+++ _next/static/webpack/static/development/pages/Panel/PanelPage.js.a15fd1426acc16732e5f.hot-update.js
@@ -0,0 +1,92 @@
+webpackHotUpdate("static/development/pages/Panel/PanelPage.js",{
+
+/***/ "./pages/Panel/PanelPage.tsx":
+/*!***********************************!*\
+  !*** ./pages/Panel/PanelPage.tsx ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @bigcommerce/big-design */ "../big-design/dist/big-design.es.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "../../node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../components */ "./components/index.tsx");
+/* harmony import */ var _PropTables__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../PropTables */ "./PropTables/index.ts");
+var _jsxFileName = "/Users/daniel.almaguer/dev/bigcommerce/big-design/packages/docs/pages/Panel/PanelPage.tsx";
+
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_1___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["H0"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "Panels"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["Text"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, "The panel component is used to contain content in a structured format.", ' ', react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["Link"], {
+    href: "https://design.bigcommerce.com/components/panels",
+    target: "_blank",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, "Panel Design Guidlines"), "."), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components__WEBPACK_IMPORTED_MODULE_2__["CodePreview"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  }, "<Panel>\n  <Flex justifyContent=\"space-between\" flexDirection=\"row\">\n    <Flex.Item>\n      <H2>Panel header</H2>\n    </Flex.Item>\n    <Flex.Item>\n      <Button variant=\"secondary\">Button</Button>\n    </Flex.Item>\n  </Flex>\n  <Text>\n    Lorem ipsum dolor amet officia humblebrag selvage, subway tile vexillologist id pickled adaptogen fashion axe.\n    Ennui meh pitchfork banh mi. Keffiyeh PBRB echo park gastropub. Pop-up neutra brunch ullamco affogato shaman\n    vexillologist quinoa post-ironic locavore. Retro selfies proident synth ethical quinoa marfa chartreuse dolor\n    vexillologist gochujang. Tempor poke unicorn, readymade crucifix fugiat culpa. Kinfolk hella asymmetrical,\n    meggings et consectetur lomo farm-to-table exercitation DIY.\n  </Text>\n  <Text>\n    Leggings consectetur heirloom, sustainable 90's scenester sartorial sint meditation portland health goth\n    bespoke gastropub authentic. Flannel iceland narwhal, laborum adaptogen paleo glossier reprehenderit. Ethical\n    ugh copper mug voluptate brooklyn velit 3 wolf moon tacos. Bushwick vexillologist air plant, veniam jianbing\n    poke semiotics wayfarers. Typewriter single-origin coffee cray, thundercats asymmetrical succulents green\n    juice irure cred taxidermy celiac.\n  </Text>\n</Panel>"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["H1"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 46
+    },
+    __self: this
+  }, "API"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["H2"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 48
+    },
+    __self: this
+  }, "Panel"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_PropTables__WEBPACK_IMPORTED_MODULE_3__["PanelPropTable"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 50
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_0__["H2"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 52
+    },
+    __self: this
+  }, "Inherited Props"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components__WEBPACK_IMPORTED_MODULE_2__["Collapsible"], {
+    title: "Margin Props",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 54
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_PropTables__WEBPACK_IMPORTED_MODULE_3__["MarginPropTable"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 55
+    },
+    __self: this
+  })));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=PanelPage.js.a15fd1426acc16732e5f.hot-update.js.map
